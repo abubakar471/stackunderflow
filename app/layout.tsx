8c5d61dbafb4f4cd502d47341320aeec2fd32fac
AB <87719025+abubakar471@users.noreply.github.com>
2026-04-11 21:24:58 +0600

    Create Next.js application with eslint and prettier configured and also shadcn initiated

---
+++ app/layout.tsx
@@ -0,0 +1,53 @@
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+
+import './globals.css';
+import ThemeProvider from '@/context/Theme';
+
+const inter = localFont({
+  src: '../public/fonts/Inter.ttf',
+  variable: '--font-inter',
+  weight: '100 200 300 400 500 600 700 800 900',
+  display: 'swap',
+});
+
+const spaceGrotesk = localFont({
+  src: '../public/fonts/SpaceGroteskVF.ttf',
+  variable: '--font-space-grotesk',
+  weight: '100 200 300 400 500 600 700 800 900',
+  display: 'swap',
+});
+
+export const metadata: Metadata = {
+  title: 'StackUnderflow',
+  description:
+    "A community for developers to engage and share their QnA. For anyone who wants to get a feel a family like environment and It's free to use",
+  icons: {
+    icon: '/images/site-logo.svg',
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${inter.variable} ${inter.className} ${spaceGrotesk.variable} h-full antialiased`}
+      suppressHydrationWarning
+    >
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
